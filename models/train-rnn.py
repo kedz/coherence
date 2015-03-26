e884021a6203c7d998b8fec8f4074fbbd7dca55e
kedz <kedzie@cs.columbia.edu>
2015-03-26 03:52:56 -0400

    Training script loads dev docs_perms to monitor loss on dev set.

--- models/train-rnn.py
+++ models/train-rnn.py
@@ -20,6 +20,10 @@ def main(output_model_path):
     docs_train = cohere.data.get_barzilay_ntsb_clean_docs_only(
         "train", tokens_only=True)
     vocab = make_vocab(docs_train)
+
+    docs_perms_dev = cohere.data.get_barzilay_ntsb_clean_docs_perms(
+        "dev", tokens_only=True)
+
     glove = cohere.embed.GloVeEmbeddings()
     embed = cohere.embed.BorrowingWordEmbeddings(vocab, glove)
     print "embbeding  '__PAD__':", pprint_emb(embed.get_embedding("__PAD__"))
@@ -34,15 +38,17 @@ def main(output_model_path):
         embed.get_embedding("__UNKNOWN__"))
     print "index      '__UNKNOWN__':", embed.get_index("__UNKNOWN__")
 
-    max_sent_len = 200
+    max_sent_len = 150
     transformer = cohere.embed.IndexDocTransformer(
         embed, start_pads=1, stop_pads=1, max_sent_len=max_sent_len)
     transformer.fit(docs_train)
     X_train, y_train = transformer.transform(docs_train)
-        
+    X_gold, X_perm = transformer.transform_test(docs_perms_dev)
+
+
     nnet = RNNModel(embed, max_sent_len, learning_rate=.1, 
                     batch_size=20, fit_init=True)
-    nnet.fit(X_train, y_train)
+    nnet.fit(X_train, y_train, X_gold=X_gold, X_perm=X_perm)
 
 
 
