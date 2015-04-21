679a920566fc0d0f8120f8922b7907cca58442ee
kedz <kedzie@cs.columbia.edu>
2015-04-21 14:11:22 -0400

    Exposed transformer and models to cohere.nnet module.

--- cohere/nnet/__init__.py
+++ cohere/nnet/__init__.py
@@ -1,5 +1,7 @@
-from cohere.nnet._transformers import TreeTransformer
+from cohere.nnet._transformers import TokensTransformer, TreeTransformer
 from cohere.nnet._embed import WordEmbeddings
 from cohere.nnet._recursive_nnet import RecursiveNNModel
+from cohere.nnet._cbow_nnet import CBOWModel
 
-__all__ = ['TreeTransformer', 'WordEmbeddings', 'RecursiveNNModel']
+__all__ = ['TokensTransformer', 'TreeTransformer', 'WordEmbeddings', 
+           'CBOWModel', 'RecursiveNNModel']
