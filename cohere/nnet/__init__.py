587f8ea6656de48ddc311dbf1ff191dba0907c70
kedz <kedzie@cs.columbia.edu>
2015-04-23 00:34:04 -0400

    Exposed RecurrentNNModel to cohere.nnet pmodule.

--- cohere/nnet/__init__.py
+++ cohere/nnet/__init__.py
@@ -1,7 +1,8 @@
 from cohere.nnet._transformers import TokensTransformer, TreeTransformer
 from cohere.nnet._embed import WordEmbeddings
-from cohere.nnet._recursive_nnet import RecursiveNNModel
 from cohere.nnet._cbow_nnet import CBOWModel
+from cohere.nnet._recurrent_nnet import RecurrentNNModel
+from cohere.nnet._recursive_nnet import RecursiveNNModel
 
 __all__ = ['TokensTransformer', 'TreeTransformer', 'WordEmbeddings', 
-           'CBOWModel', 'RecursiveNNModel']
+           'CBOWModel', 'RecurrentNNModel', 'RecursiveNNModel']
