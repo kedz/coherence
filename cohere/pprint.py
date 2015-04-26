3b5c517cd7d82f621a25e2e6c472d631df0033f8
Chris Kedzie <kedzie@cs.columbia.edu>
2015-04-26 18:18:08 -0400

    Added pretty printing for tree CoherenceInstances.

--- cohere/pprint.py
+++ cohere/pprint.py
@@ -9,8 +9,12 @@ def _print_str_dataset(dataset):
 def _print_str_instance(instance):
     string = u"[ " + unicode(instance.id)
     string += u" w/ {} permutations\n".format(instance.num_perms)
-    string += u"\n".join([" " + unicode(idx) + ") " + u" ".join(sent) 
-                          for idx, sent in enumerate(instance.gold, 1)])
+    if instance.format == "tokens":
+        string += u"\n".join([" " + unicode(idx) + ") " + u" ".join(sent) 
+                              for idx, sent in enumerate(instance.gold, 1)])
+    elif instance.format == "trees":
+        string += u"\n".join([" {}) {}".format(idx, u" ".join(sent.leaves()))
+                              for idx, sent in enumerate(instance.gold, 1)])
     string += u"]\n"
     return string
 
