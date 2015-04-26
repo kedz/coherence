3f8af247c07e1869be61bdf2582fa7297f5e7668
Chris Kedzie <kedzie@cs.columbia.edu>
2015-04-26 00:14:05 -0400

    Added a pretty printing module for dataset.

---
+++ cohere/pprint.py
@@ -0,0 +1,21 @@
+import cohere.data
+import corenlp.document
+
+def _print_str_dataset(dataset):
+    return u"\n".join([_print_str_instance(inst) 
+                       for idx, inst in enumerate(dataset)])
+        
+
+def _print_str_instance(instance):
+    string = u"[ " + unicode(instance.id)
+    string += u" w/ {} permutations\n".format(instance.num_perms)
+    string += u"\n".join([" " + unicode(idx) + ") " + u" ".join(sent) 
+                          for idx, sent in enumerate(instance.gold, 1)])
+    string += u"]\n"
+    return string
+
+def pprint(obj):
+    if isinstance(obj, cohere.data.CoherenceData):
+        print _print_str_dataset(obj).encode(u"utf-8")
+    elif isinstance(obj, cohere.data.CoherenceInstance):
+        print _print_str_instance(obj).encode(u"utf-8")
