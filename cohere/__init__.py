47ef03106699dfd8e0358b6dd6ac7f832dbe4d7a
Chris Kedzie <kedzie@cs.columbia.edu>
2015-04-28 15:34:03 -0400

    Made pretty printing part of main import.

---
+++ cohere/__init__.py
@@ -0,0 +1,3 @@
+from _pprint import pprint, pprint_X
+
+__all__ = ["pprint", "pprint_X"]
--- cohere/_pprint.py
+++ cohere/_pprint.py
@@ -23,3 +23,31 @@ def pprint(obj):
         print _print_str_dataset(obj).encode(u"utf-8")
     elif isinstance(obj, cohere.data.CoherenceInstance):
         print _print_str_instance(obj).encode(u"utf-8")
+
+def pprint_X(X, transformer, width=80):
+    
+    word_dim = transformer.embeddings.W.shape[1]
+    win_size = transformer.window_size
+    max_words = transformer.max_sent_len
+    win_width = int(width - 5 * (win_size -1)) / int(win_size)
+
+    if len(X.shape) == 1:
+        X = X.reshape((1, X.shape[0]))
+    if X.shape[1] != max_words * win_size:
+        raise Exception(
+            "X columns {} not consistent with transfomer {}".format(
+                X.shape[1], max_words * win_size))
+
+    for x in X:
+        cols = []
+        for k in xrange(win_size):
+            words = []
+            for i in x[k * max_words : (k + 1) * max_words]:
+                if i != -1:
+                    words.append(transformer.embeddings.index2token[i])
+            line = u' '.join(words)[:win_width]
+            line += u' ' * (win_width - len(line))
+            cols.append(line)
+        print u" \u22EF  ".join(cols).encode(u"utf-8")    
+
+
